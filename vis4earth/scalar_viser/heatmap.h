8369a3e473417cbe4a3c52a737580359f4eb7295
Camel <camel_exvl@outlook.com>
2024-07-21 21:01:32 +0800

    将2D热力图计算迁移至GPU

--- vis4earth/scalar_viser/heatmap.h
+++ vis4earth/scalar_viser/heatmap.h
@@ -5,6 +5,7 @@
 #include <osg/Group>
 #include <osg/ShapeDrawable>
 
+#include <vis4earth/compute_shader.h>
 #include <vis4earth/geographics_cmpt.h>
 #include <vis4earth/osg_util.h>
 #include <vis4earth/qt_osg_reflectable.h>
@@ -26,6 +27,8 @@ class HeatmapRenderer : public QtOSGReflectableWidget {
 
     osg::ref_ptr<osg::Group> GetGroup() const { return grp; }
 
+    void emitUpdateHeatmap2DSignal() { emit updateHeatmap2DSignal(); }
+
   protected:
     osg::ref_ptr<osg::Group> grp;
     osg::ref_ptr<osg::Geometry> geom;
@@ -45,6 +48,29 @@ class HeatmapRenderer : public QtOSGReflectableWidget {
     void initOSGResource();
     void updateHeatmap2D();
     void updateGeometry();
+
+  signals:
+    void updateHeatmap2DSignal();
+};
+
+class Heatmap2DDrawCallback : public QObject, public osg::Drawable::DrawCallback {
+    Q_OBJECT
+  public:
+    Heatmap2DDrawCallback(HeatmapRenderer *heatmapRenderer, Ui::HeatmapRenderer *&ui,
+                          osg::ref_ptr<osg::Texture2D> &volSliceTex, VolumeComponent &volCmpt,
+                          QImage &heatmap2D);
+    ~Heatmap2DDrawCallback() override { delete computeShader; }
+    void drawImplementation(osg::RenderInfo &renderInfo,
+                            const osg::Drawable *drawable) const override;
+
+  protected:
+    HeatmapRenderer *heatmapRenderer;
+    Ui::HeatmapRenderer *&ui;
+    osg::ref_ptr<osg::Texture2D> &volSliceTex;
+    VolumeComponent &volCmpt;
+    QImage &heatmap2D;
+
+    ComputeShader *computeShader;
 };
 
 } // namespace VIS4Earth
