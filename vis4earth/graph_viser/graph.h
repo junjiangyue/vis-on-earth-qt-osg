2499077c7281cc3f1f8a25775feb15634b714cf9
junjiangyue <1391598941@qq.com>
2024-10-22 21:08:51 +0800

    图布局完善

--- vis4earth/graph_viser/graph.h
+++ vis4earth/graph_viser/graph.h
@@ -316,10 +316,10 @@ struct Graph {
   public:
     Graph()
         : K(0.000001), I(10), iter(I), cycles(6), compatibilityThreshold(0.6), smoothWidth(30.0),
-          S(0.000001), edgeDistance(1e-4), gravitationIsOn(false),
-          gravitationCenter(-900.0, -400.0, 0), gravitationExponent(-2.0),
+          S(0.000001), edgeDistance(1e-4), gravitationIsOn(true),
+          gravitationCenter(40.0, 0.0, 0), gravitationExponent(-2.0),
           edgeWeightThreshold(-1.0), edgePercentageThreshold(-1.0), edgeOpacity(0.1),
-          nodeRestrictionIsOn(false), repulsion(0.2), spring_k(5.4), attraction(0.32),
+          nodeRestrictionIsOn(false), repulsion(0.2), spring_k(5.4), attraction(25.1),
           n_iterations(0), edgeLength(50) {}
 
     int getLayoutIteration() const { return n_iterations; }
